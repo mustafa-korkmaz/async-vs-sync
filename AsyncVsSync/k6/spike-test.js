e525bae5ecb515f557e0c34148a0dc1dc18cd71b
Mustafa Korkmaz <m.korkmaz@outlook.com>
2022-12-08 16:08:23 +0200

    format js content

--- AsyncVsSync/k6/spike-test.js
+++ AsyncVsSync/k6/spike-test.js
@@ -1,19 +1,11 @@
 ﻿import http from 'k6/http';
 
 export const options = {
-    stages: [{
-        duration: '30s', //spike to 2500 users in 30 secs
-        target: 2500
-    },
-     {
-        duration: '90s', //stay at 2500 users for 1.5 mins
-        target: 2500
-    }, 
-    {
-        duration: '30s', //scale down to 1000 users
-        target: 1000
-    }, 
-],
+    stages: [
+    { duration: '30s', target: 2500 }, //spike to 2500 users in 30 secs  
+    { duration: '90s', target: 2500 }, //stay at 2500 users for 1.5 mins
+    { duration: '30s', target: 1000 }  //scale down to 1000 users
+]
 };
 
 export default function () {
--- AsyncVsSync/k6/stress-test.js
+++ AsyncVsSync/k6/stress-test.js
@@ -1,15 +1,10 @@
 ﻿import http from 'k6/http';
 
 export const options = {
-    stages: [{
-            duration: '2m', //scale up to 2500 users in 2 mins
-            target: 2500
-        },
-        {
-            duration: '30s', //stay at 2500 users for 30 secs
-            target: 2500
-        }
-    ],
+    stages: [
+    { duration: '2m', target: 2500 }, //scale up to 2500 users in 2 mins
+    { duration: '30s', target: 2500 } //stay at 2500 users for 30 secs
+]
 };
 
 export default function () {
